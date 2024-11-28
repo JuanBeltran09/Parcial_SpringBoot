9de24c1db1d6e5bd953e431786c76b0c061c7cd2
Juan Beltran <beltranrodriguezjuan9@gmail.com>
2024-11-28 18:27:13 -0500

    Correction

---
+++ src/main/java/uptc/edu/co/Parcial_SpringBoot/ParcialSpringBootApplication.java
@@ -0,0 +1,13 @@
+package uptc.edu.co.Parcial_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uptc/edu/co/Parcial_SpringBoot/ParcialSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package uptc.edu.co.Parcial_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
