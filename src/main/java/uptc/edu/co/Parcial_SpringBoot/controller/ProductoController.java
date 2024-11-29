86382ac0f923ee14607fbb6d736b5f9e457d2d39
Juan Beltran <beltranrodriguezjuan9@gmail.com>
2024-11-28 20:45:27 -0500

    Product Controller

--- src/main/java/uptc/edu/co/Parcial_SpringBoot/controller/ProductoController.java
+++ src/main/java/uptc/edu/co/Parcial_SpringBoot/controller/ProductoController.java
@@ -1,4 +1,43 @@
 package uptc.edu.co.Parcial_SpringBoot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uptc.edu.co.Parcial_SpringBoot.entityes.Producto;
+import uptc.edu.co.Parcial_SpringBoot.handling.ResponseHandler;
+import uptc.edu.co.Parcial_SpringBoot.service.ProductoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public ResponseEntity<Object> getProductos() {
+        try {
+            List<Producto> result = productoService.getProductos();
+            return new ResponseHandler().generateResponse("Productos obtenidos con éxito", HttpStatus.OK, result);
+        } catch (Exception e) {
+            return new ResponseHandler().generateResponse("Error al obtener los productos", HttpStatus.INTERNAL_SERVER_ERROR, null);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody Producto producto) {
+        if (producto == null) {
+            return new ResponseHandler().generateResponse("Producto no puede ser nulo", HttpStatus.BAD_REQUEST, null);
+        }
+        try {
+            Producto result = productoService.save(producto);
+            return new ResponseHandler().generateResponse("Producto creado con éxito", HttpStatus.CREATED, result);
+        } catch (Exception e) {
+            return new ResponseHandler().generateResponse("Error al crear el producto", HttpStatus.INTERNAL_SERVER_ERROR, null);
+        }
+    }
+
 }
--- src/main/java/uptc/edu/co/Parcial_SpringBoot/controller/VentaController.java
+++ src/main/java/uptc/edu/co/Parcial_SpringBoot/controller/VentaController.java
@@ -4,6 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uptc.edu.co.Parcial_SpringBoot.entityes.Cliente;
+import uptc.edu.co.Parcial_SpringBoot.entityes.Producto;
+import uptc.edu.co.Parcial_SpringBoot.entityes.Venta;
+import uptc.edu.co.Parcial_SpringBoot.handling.ResponseHandler;
+import uptc.edu.co.Parcial_SpringBoot.service.VentaService;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/ventas")
 public class VentaController {
+
+    @Autowired
+    private VentaService ventaService;
+
+    @GetMapping("/clientes")
+    public ResponseEntity<Object> getClientes() {
+        try {
+            List<Cliente> result = ventaService.getClientes();
+            return new ResponseHandler().generateResponse("Clientes obtenidos con éxito", HttpStatus.OK, result);
+        } catch (Exception e) {
+            return new ResponseHandler().generateResponse("Error al obtener los clientes", HttpStatus.INTERNAL_SERVER_ERROR, null);
+        }
+    }
+
+    @GetMapping("/productos")
+    public ResponseEntity<Object> getProductos() {
+        try {
+            List<Producto> result = ventaService.getProductos();
+            return new ResponseHandler().generateResponse("Productos obtenidos con éxito", HttpStatus.OK, result);
+        } catch (Exception e) {
+            return new ResponseHandler().generateResponse("Error al obtener los productos", HttpStatus.INTERNAL_SERVER_ERROR, null);
+        }
+    }
+
 }
